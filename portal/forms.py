b183e9f4cae54463a578f7624edcf69c7e52eaa1
Arad Firouzkouhi <aradfir@gmail.com>
2022-08-02 16:53:02 +0430

    added contact us form

--- portal/forms.py
+++ portal/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class RegisterForm(forms.ModelForm):
@@ -24,7 +25,7 @@ class RegisterForm(forms.ModelForm):
         confirm_password = cleaned_data.get("confirm_password")
 
         if password != confirm_password:
-            self.add_error('confirm_password', "Password and Password Repeat do not match! CUSTOM" )
+            self.add_error('confirm_password', "Password and Password Repeat do not match! CUSTOM")
         return cleaned_data
 
     def save(self, commit=True):
@@ -38,3 +39,10 @@ class RegisterForm(forms.ModelForm):
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=150)
     password = forms.CharField(widget=forms.PasswordInput())
+
+
+class ContactForm(forms.Form):
+    title = forms.CharField(max_length=150)
+    email = forms.EmailField()
+    text = forms.CharField(widget=forms.Textarea, validators=[MinValueValidator(10, 'Message Too Short!'),
+                                                              MaxValueValidator(250, "Message Too Long!")])
