e8318ad08c7e5dd646ac38beb3291d7714be1edf
Arad Firouzkouhi <aradfir@gmail.com>
2022-08-03 11:02:11 +0430

    add course model

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Course(models.Model):
+    department = models.CharField(max_length=150)
+    name = models.CharField(max_length=150)
+    course_number = models.IntegerField()
+    professor_name = models.CharField(max_length=150)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    group = models.IntegerField()
+    DAYS_OF_WEEK = (
+        (0, 'Saturday'),
+        (1, 'Sunday'),
+        (2, 'Monday'),
+        (3, 'Tuesday'),
+        (4, 'Wednesday'),
+    )
+
+    day_1 = models.IntegerField(choices=DAYS_OF_WEEK, null=False)
+    day_2 = models.IntegerField(choices=DAYS_OF_WEEK, null=True)
